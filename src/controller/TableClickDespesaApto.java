fb85a081a328b18944b5b0380e13e576e3eb3a36
Denys <denys.wyliam@gmail.com>
2016-06-10 22:25:26 -0300

    Tela despesas do apartamento e crud concluidos.

---
+++ src/controller/TableClickDespesaApto.java
@@ -0,0 +1,69 @@
+package controller;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public class TableClickDespesaApto implements MouseListener {
+
+	private JTable table;
+	private DefaultTableModel model;
+	private JTextField txtDespesa;
+	private JTextField txtRef;
+	private JTextField txtVencimento;
+	private JTextField txtValor;
+	private JTextField idDespesa;
+	private JButton btnAtualizarDespesa;
+
+	public TableClickDespesaApto(JTable table, DefaultTableModel model, JTextField txtDespesa, JTextField txtRef,
+			JTextField txtVencimento, JTextField txtValor, JTextField idDespesa, JButton btnAtualizarDespesa) {
+		this.table = table;
+		this.model = model;
+		this.txtDespesa = txtDespesa;
+		this.txtRef = txtRef;
+		this.txtVencimento = txtVencimento;
+		this.txtValor = txtValor;
+		this.idDespesa = idDespesa;
+		this.btnAtualizarDespesa = btnAtualizarDespesa;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		int linha = table.rowAtPoint(e.getPoint());
+		txtDespesa.setText(model.getValueAt(linha, 1).toString());
+		txtRef.setText(model.getValueAt(linha, 2).toString());
+		txtVencimento.setText(model.getValueAt(linha, 3).toString());
+		txtValor.setText(model.getValueAt(linha, 4).toString());
+		idDespesa.setText(model.getValueAt(linha, 0).toString());
+		btnAtualizarDespesa.setEnabled(true);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/persistence/DespesasApartamentoDao.java
+++ src/persistence/DespesasApartamentoDao.java
@@ -10,77 +10,76 @@ import java.util.List;
 import model.Despesas;
 
 public class DespesasApartamentoDao implements IDespesasApartamentoDao {
-	
+
 	private Connection c;
-	
-	public DespesasApartamentoDao(){
+
+	public DespesasApartamentoDao() {
 		GenericDao gDao = new GenericDao();
 		c = gDao.getConnection();
 	}
 
-
-
-	@Override		
+	@Override
 	public void atualizaDespesaApartamento(Despesas despesa) throws SQLException {
 		String sql = "UPDATE despesa_apartamento SET despesa = ?, valor = ?, "
-				+ "dtVencimento = ?, dtReferencia = ?, WHERE id = ?";
+				+ "dtVencimento = ?, dtReferencia = ?  WHERE id = ?";
 		PreparedStatement ps = c.prepareStatement(sql);
 		ps.setString(1, despesa.getDespesa());
 		ps.setFloat(2, despesa.getValor());
 		ps.setString(3, despesa.getDtVencimento());
 		ps.setString(4, despesa.getDtReferencia());
 		ps.setInt(5, despesa.getId());
-		
+
 		ps.execute();
 		ps.close();
 	}
 
 	@Override
-	public void excluiDespesaApartamento(Despesas despesa) throws SQLException {
+	public void excluiDespesaApartamento(List<Integer> d) throws SQLException {
 		String sql = "DELETE FROM despesa_apartamento WHERE id = ?";
 		PreparedStatement ps = c.prepareStatement(sql);
-		ps.setInt(1, despesa.getId());
-		
-		ps.execute();
+
+		// for (int i = 0; i < despesas.length; i++) {
+		// ps.setInt(1, despesas[i]);
+		// ps.execute();
+		// }
+		for (int i = 0; i < d.size(); i++) {
+			ps.setInt(1, d.get(i));
+			ps.execute();
+		}
+
 		ps.close();
 	}
-
-
-
 
 	@Override
 	public void insereDespesaApartamento(Despesas despesa, int idApartamento) throws SQLException {
 		String sql = "INSERT INTO despesa_apartamento "
-				+ "(id_apartamento, despesa, valor, dtVencimento, dtReferencia) "
-				+ "VALUES (?,?,?,?,?)";
+				+ "(id_apartamento, despesa, valor, dtVencimento, dtReferencia) " + "VALUES (?,?,?,?,?)";
 		PreparedStatement ps = c.prepareStatement(sql);
 		ps.setInt(1, idApartamento);
 		ps.setString(2, despesa.getDespesa());
 		ps.setFloat(3, despesa.getValor());
 		ps.setString(4, despesa.getDtVencimento());
 		ps.setString(5, despesa.getDtReferencia());
-		
-		
+
 		ps.execute();
 		ps.close();
-		
+
 	}
 
+	// DATE_FORMAT( `date` , '%d/%c/%Y %H:%i:%s' )
+	// select( 'DB::raw('DATE_FORMAT(my_date, \'%m-%d-%Y\') AS arrival')' );
 	@Override
-	public List<Despesas> consultaDespesasApartamento(int numApto) throws SQLException {
-		String sql = "SELECT da.id, da.despesa, da.valor, da.dtVencimento, da.dtReferencia"
-				+ " FROM despesa_apartamento da, apartamento a "
-				+ "WHERE da.id_apartamento = a.id AND a.numero = ?";
+	public List<Despesas> consultaDespesasApartamento(int idApto) throws SQLException {
+		String sql = "SELECT da.id, da.despesa, da.valor, da.dtVencimento, " + "da.dtReferencia"
+				+ " FROM despesa_apartamento da, apartamento a " + " WHERE da.id_apartamento = a.id AND a.id = ?";
 		PreparedStatement ps = c.prepareStatement(sql);
-		
-		ps.setInt(1, numApto);
-		
+
+		ps.setInt(1, idApto);
+
 		ResultSet rs = ps.executeQuery();
 		List<Despesas> ListaDespesas = new ArrayList<Despesas>();
-		
-		
-		
-		while(rs.next()){
+
+		while (rs.next()) {
 			Despesas d = new Despesas();
 			d.setId(rs.getInt("id"));
 			d.setDespesa(rs.getString("despesa"));
@@ -89,7 +88,7 @@ public class DespesasApartamentoDao implements IDespesasApartamentoDao {
 			d.setDtReferencia(rs.getString("dtReferencia"));
 			ListaDespesas.add(d);
 		}
-		
+
 		rs.close();
 		ps.close();
 		return ListaDespesas;
