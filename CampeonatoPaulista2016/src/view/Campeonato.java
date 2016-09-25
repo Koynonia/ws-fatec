ab2517788ad5206c2200dc08a45911ed99651b57
Fernando <koynonia@gmail.com>
2016-09-25 16:41:09 -0300

    Layout da Tela de Grupos

--- CampeonatoPaulista2016/src/view/Campeonato.java
+++ CampeonatoPaulista2016/src/view/Campeonato.java
@@ -5,7 +5,13 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.JLabel;
 
 public class Campeonato extends JFrame {
 
@@ -14,6 +20,10 @@ public class Campeonato extends JFrame {
 	 */
 	private static final long serialVersionUID = 6628613480798698487L;
 	private JPanel contentPane;
+	private JTable tbGrupoA;
+	private JTable tbGrupoB;
+	private JTable tbGrupoC;
+	private JTable tbGrupoD;
 
 	/**
 	 * Launch the application.
@@ -35,12 +45,52 @@ public class Campeonato extends JFrame {
 	 * Create the frame.
 	 */
 	public Campeonato() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		
+		setBounds(100, 100, 560, 400);
+		setTitle("Geração de Grupos");
+		setName("Grupos");
+		setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
+		setLocationRelativeTo(null);
+		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton btnGerarGrupos = new JButton("Gerar Grupos");
+		btnGerarGrupos.setBounds(218, 332, 126, 29);
+		btnGerarGrupos.setVisible(true);
+		contentPane.setLayout(null);
+		contentPane.add(btnGerarGrupos);
+		
+		tbGrupoA = new JTable();
+		tbGrupoA.setBounds(22, 48, 206, 105);
+		contentPane.add(tbGrupoA);
+		
+		tbGrupoB = new JTable();
+		tbGrupoB.setBounds(330, 48, 206, 105);
+		contentPane.add(tbGrupoB);
+		
+		tbGrupoC = new JTable();
+		tbGrupoC.setBounds(22, 201, 206, 105);
+		contentPane.add(tbGrupoC);
+		
+		tbGrupoD = new JTable();
+		tbGrupoD.setBounds(330, 201, 206, 105);
+		contentPane.add(tbGrupoD);
+		
+		JLabel lblGrupoA = new JLabel("Grupo A");
+		lblGrupoA.setBounds(92, 23, 61, 16);
+		contentPane.add(lblGrupoA);
+		
+		JLabel lblGrupoB = new JLabel("Grupo B");
+		lblGrupoB.setBounds(407, 23, 61, 16);
+		contentPane.add(lblGrupoB);
+		
+		JLabel lblGrupoC = new JLabel("Grupo C");
+		lblGrupoC.setBounds(92, 173, 61, 16);
+		contentPane.add(lblGrupoC);
+		
+		JLabel lblGrupoD = new JLabel("Grupo D");
+		lblGrupoD.setBounds(407, 173, 61, 16);
+		contentPane.add(lblGrupoD);
 	}
-
 }
